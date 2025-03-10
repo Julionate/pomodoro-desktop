9f7f4de1b960db2a5df366b0ebb9074110dab76c
Julionate <julionate@proton.me>
2025-03-10 20:10:31 -0300

    Project upload.

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./styles/global.css";
+
+function App() {
+  return (
+    <main class="container">
+      <h1>This template includes TailwindCSS already configured!</h1>
+    </main>
+  );
+}
+
+export default App;
